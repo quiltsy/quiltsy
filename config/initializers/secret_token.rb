0050cc2b06a2ed493722a688df6202387f7495b2
Alexander M Dixon <alexander.m.dixon@gmail.com>
2014-11-01 11:13:59 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quiltsy::Application.config.secret_key_base = '673bbb73d2d39f884dc9e1cde340b0dacbf350e0b8075fc31423517d1e2bdca3c2da7c4c2650bc3574d8717cef7836ec44babc4d698fae668a528011c8de717a'
